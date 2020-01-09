cf139314d964fbf3b0b68c5881b8ed1d948322de
deta15 <deta.mihai@gmail.com>
2020-01-09 19:24:39 +0200

    bash script changed for necessary folder and files

--- vagrant_scripted_settings/provision.sh
+++ vagrant_scripted_settings/provision.sh
@@ -7,18 +7,29 @@ PASSWORD='mypass'
 sudo mkdir "/var/www/"
 
 # update / upgrade
-sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt update
+sudo apt -y upgrade
 
-# install apache 2.5 and php 5.5
-sudo apt-get install -y apache2
-sudo apt-get install -y php5
+sudo apt install -y ca-certificates apt-transport-https
+wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
+echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
+sudo apt update
+sudo apt install -y php7.3
+sudo apt install -y php7.3-cli php7.3-common php7.3-curl php7.3-mbstring php7.3-mysql php7.3-xml php7.3-zip php7.3-Intl php7.3-mysqli
+
+sudo apt install -y php7.2
+sudo apt install -y php7.2-cli php7.2-common php7.2-curl php7.2-mbstring php7.2-mysql php7.2-xml php7.2-zip php7.2-Intl php7.2-mysqli
+
+sudo apt install -y php7.1
+sudo apt install -y php7.1-cli php7.1-common php7.1-curl php7.1-mbstring php7.1-mysql php7.1-xml php7.1-zip php7.1-Intl php7.1-mysqli
+
+sudo apt install -y mysql-server
+
+sudo apt install -y apache2 libapache2-mod-php7.1 libapache2-mod-php7.2 libapache2-mod-php7.3
 
 # install mysql and give password to installer
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
-sudo apt-get -y install mysql-server
-sudo apt-get -y install php5-mysql
 
 # install phpmyadmin and give password(s) to installer
 # for simplicity I'm using the same password for mysql and phpmyadmin
@@ -27,7 +38,7 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm pass
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo apt-get -y install phpmyadmin
+sudo apt install -y phpmyadmin php-mbstring php-gettext
 
 # setup hosts file
 VHOST=$(cat <<EOF
@@ -40,17 +51,26 @@ VHOST=$(cat <<EOF
 </VirtualHost>
 EOF
 )
-echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+mkdir serverconfig
+touch /serverconfig/vhosts.conf
+
+sudo ln -s /etc/apache2/sites-enabled/000-default.conf /serverconfig/vhosts.conf
+
+sudo echo "${VHOST}" > /serverconfig/vhosts.conf
 
 # enable mod_rewrite
 sudo a2enmod rewrite
 
 # restart apache
-service apache2 restart
+sudo service apache2 restart
+
+#make posible changin the php version
+sudo apt install -y software-properties-common
 
 # install git
-sudo apt-get -y install git
+sudo apt -y install git
 
 # install Composer
-curl -s https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+cd /var/www && curl -s https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
